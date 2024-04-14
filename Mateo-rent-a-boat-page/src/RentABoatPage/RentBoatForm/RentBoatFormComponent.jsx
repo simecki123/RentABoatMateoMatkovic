3ec7db4398e9bb0cf0d24ff065b1e2134628bac4
Sime Roncevic <sroncevic19@gmail.com>
2024-04-14 21:39:03 +0200

    Form Functionality works

--- Mateo-rent-a-boat-page/src/RentABoatPage/RentBoatForm/RentBoatFormComponent.jsx
+++ Mateo-rent-a-boat-page/src/RentABoatPage/RentBoatForm/RentBoatFormComponent.jsx
@@ -1,39 +1,60 @@
+import React, { useState } from 'react';
 import './RentBoatFormStyle.css';
 import logo from '../../assets/MatinLogo.png';
 
 function RentABoatFormComponent() {
-    return(
+    const [selectedDate, setSelectedDate] = useState('');
+
+    const handleDateChange = (event) => {
+        const selectedDateString = event.target.value;
+        setSelectedDate(selectedDateString);
+    };
+
+    return (
         <>
             <div className='rent-aboat-form-container'>
                 <h2 className='rent-aboat-form-title'>Rent this boat</h2>
-                <form className='rent-aboat-form'>
+                <form className='rent-aboat-form' action="https://api.web3forms.com/submit" method="POST">
                     <div className='important-data-and-logo-container'>
                         <div className='important-data-container'>
                             <label className='input-label'>Name and surname</label>
-                            <input type='text' placeholder='John Snow' />
+                            <input name='Name and surname' type='text' placeholder='John Snow' />
                             <label className='input-label'>E-mail</label>
-                            <input type="text" placeholder='something@gmail.com' />
+                            <input name='email' type="text" placeholder='something@gmail.com' />
                             <label className='input-label'>Mobile number</label>
-                            <input type='number' placeholder='095-356-587' />
+                            <input name='phone-number' type='number' placeholder='095-356-587' />
                             <label className='input-label'>Date</label>
-                            <input type='date' placeholder='07/07/2024' />
+                            {/* Visible date input allowing user selection */}
+                            <input
+                                type='date'
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                            />
+                            {/* Hidden input to store selected date for form submission */}
+                            <input
+                                name='rent-date'
+                                type='hidden'
+                                value={selectedDate}
+                            />
+                            {/* Access key for Web3Forms API (assuming it's required) */}
+                            <input
+                                type="hidden"
+                                name="access_key"
+                                value="07e223af-d705-4551-a5f6-fcd705f6d17b"
+                            />
                         </div>
                         <div className='logo-container'>
-                            <img className='logo-in-form' src={logo} />
+                            <img className='logo-in-form' src={logo} alt="Logo" />
                         </div>
                     </div>
                     <div className='message-checkbox-button-container'>
                         <label className='input-label'>Message</label>
-                        <textarea placeholder='Write important additonal message here for us.' />
-                        <input type='checkbox'id="checkbox1" name="iagreecheckbox" value="Agreed" />
-                        <label > I Agree</label>
-                        <p>By sending an inquiry through this form, you acknowledge you are familiar
-                             with the privacy policy and agree with it. With this you also give consent
-                              (permission) for the collection and processing of personal data specified
-                               in the form, and which are necessary for us to be able to accommodate your
-                                request.</p>
+                        <textarea name='additional-message' placeholder='Write important additional message here for us.' />
+                        <input type='checkbox' id="checkbox1" name="iagree-checkbox" value="Agreed" />
+                        <label htmlFor="checkbox1">I Agree</label>
+                        <p>By sending an inquiry through this form, you acknowledge you are familiar with the privacy policy and agree with it. With this, you also give consent (permission) for the collection and processing of personal data specified in the form, and which are necessary for us to be able to accommodate your request.</p>
                     </div>
-                    <button className='book-the-boat'>Book the boat</button>
+                    <button type='submit' className='book-the-boat'>Book the boat</button>
                 </form>
             </div>
         </>

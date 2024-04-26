6d9428a2bd49d0e600eb023aa0439186fb86b654
Sime Roncevic <sroncevic19@gmail.com>
2024-04-26 18:25:11 +0200

    Updating images

--- Mateo-rent-a-boat-page/src/boatsDataFolder/boatsData.js
+++ Mateo-rent-a-boat-page/src/boatsDataFolder/boatsData.js
@@ -5,6 +5,43 @@ import boat3 from '../assets/_DSC4810.jpg';
 import boat4 from '../assets/_DSC4831.jpg';
 import boat5 from '../assets/_DSC4839.jpg';
 import boat6 from '../assets/_DSC4857.jpg';
+import boat7 from '../assets/_DSC4791.jpg';
+import boat8 from '../assets/_DSC4804.jpg';
+import boat9 from '../assets/_DSC4810.jpg';
+import boat10 from '../assets/_DSC4831.jpg';
+import boat11 from '../assets/_DSC4839.jpg';
+import boat12 from '../assets/_DSC4857.jpg';
+import boat13 from '../assets/_DSC4862.jpg';
+import boat14 from '../assets/_DSC4875.jpg';
+import boat15 from '../assets/_DSC4877.jpg';
+import boat16 from '../assets/_DSC4878.jpg';
+import boat17 from '../assets/_DSC4888.jpg';
+import boat18 from '../assets/_DSC4901.jpg';
+import boat19 from '../assets/_DSC4903.jpg';
+import boat20 from '../assets/_DSC4906.jpg';
+import boat21 from '../assets/_DSC4919.jpg';
+import boat22 from '../assets/_DSC4924.jpg';
+import boat23 from '../assets/_DSC4931.jpg';
+import boat24 from '../assets/_DSC4950.jpg';
+import boat25 from '../assets/_DSC4958.jpg';
+import boat26 from '../assets/_DSC4972.jpg';
+import boat27 from '../assets/_DSC4976.jpg';
+import boat28 from '../assets/_DSC4980.jpg';
+import boat29 from '../assets/DJI_0784.jpg';
+import boat30 from '../assets/DJI_0787.jpg';
+import boat31 from '../assets/DJI_0795.jpg';
+import boat32 from '../assets/DJI_0799.jpg';
+import boat33 from '../assets/DJI_0800.jpg';
+import boat34 from '../assets/DJI_0803.jpg';
+import boat35 from '../assets/DJI_0805.jpg';
+import boat36 from '../assets/DJI_0808.jpg';
+import boat37 from '../assets/DJI_0810.jpg';
+import boat38 from '../assets/DJI_0814.jpg';
+import boat39 from '../assets/DJI_0815.jpg';
+import boat40 from '../assets/DJI_0836.jpg';
+import boat41 from '../assets/DJI_0844.jpg';
+
+
 
 import video1 from '../assets/MatinBrod2.mp4';
 
@@ -59,7 +96,43 @@ const boatsData = [
         "boat4": boat4,
         "boat5": boat5,
         "boat6": boat6,
-      },
+        "boat7": boat7,
+        "boat8": boat8,
+        "boat9": boat9,
+        "boat10": boat10,
+        "boat11": boat11,
+        "boat12": boat12,
+        "boat13": boat13,
+        "boat14": boat14,
+        "boat15": boat15,
+        "boat16": boat16,
+        "boat17": boat17,
+        "boat18": boat18,
+        "boat19": boat19,
+        "boat20": boat20,
+        "boat21": boat21,
+        "boat22": boat22,
+        "boat23": boat23,
+        "boat24": boat24,
+        "boat25": boat25,
+        "boat26": boat26,
+        "boat27": boat27,
+        "boat28": boat28,
+        "boat29": boat29,
+        "boat30": boat30,
+        "boat31": boat31,
+        "boat32": boat32,
+        "boat33": boat33,
+        "boat34": boat34,
+        "boat35": boat35,
+        "boat36": boat36,
+        "boat37": boat37,
+        "boat38": boat38,
+        "boat39": boat39,
+        "boat40": boat40,
+        "boat41": boat41,
+    },
+    
 
       videos: {
         "video1": video1,
